9754df1c44c5970c93be4fee21687e63b7113993
idk <problemsolver@openmailbox.org>
2018-11-02 15:03:50 -0400

    Is this a ridiculous idea?

--- firefox.launchers/windows/i2pbrowser-private.bat
+++ firefox.launchers/windows/i2pbrowser-private.bat
@@ -1,3 +1,9 @@
 @echo off
-start "" "C:\Program Files {x86}\Mozilla Firefox\firefox.exe" -no-remote -profile ".\firefox.profile.i2p" -private-window about:blank
+if exist "C:\Program Files\Mozilla Firefox\firefox.exe" (
+    start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -no-remote -profile ".\firefox.profile.i2p" -private-window about:blank
+) else (
+    if exist "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" (
+        start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -no-remote -profile ".\firefox.profile.i2p" -private-window about:blank
+    )
+)
 pause
--- firefox.launchers/windows/i2pbrowser.bat
+++ firefox.launchers/windows/i2pbrowser.bat
@@ -1,3 +1,9 @@
 @echo off
-start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -no-remote -profile ".\firefox.profile.i2p"
+if exist "C:\Program Files\Mozilla Firefox\firefox.exe" (
+    start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -no-remote -profile ".\firefox.profile.i2p"
+) else (
+    if exist "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" (
+        start "" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -no-remote -profile ".\firefox.profile.i2p"
+    )
+)
 pause
