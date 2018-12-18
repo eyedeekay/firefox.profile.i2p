17235fcfbe04ecd41fe7f80539caf7093c183abf
idk <problemsolver@openmailbox.org>
2018-12-18 02:47:27 -0500

    linux improvements, make a snap

--- firefox.launchers/gnulinux/I2PBrowser-Private.sh
+++ firefox.launchers/gnulinux/I2PBrowser-Private.sh
@@ -2,4 +2,5 @@
 
 DIR=$(who am i | awk '{print $1}')/.i2pbrowser/
 
-"$DIR/install.sh" private $@
+"$DIR/install.sh" install
+"$DIR/install.sh" private
--- firefox.launchers/gnulinux/I2PBrowser.sh
+++ firefox.launchers/gnulinux/I2PBrowser.sh
@@ -2,4 +2,5 @@
 
 DIR=$(who am i | awk '{print $1}')/.mozilla/firefox/firefox.profile.i2p
 
-"$DIR/install.sh" run $@
+"$DIR/install.sh" install
+"$DIR/install.sh" run
