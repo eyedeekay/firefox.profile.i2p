88971dff70285a45feccf3b112c177763bd2690e
idk <problemsolver@openmailbox.org>
2018-12-28 21:41:04 -0500

    change up the launchers to tolerate the snap better.

--- firefox.launchers/gnulinux/I2PBrowser-Private.sh
+++ firefox.launchers/gnulinux/I2PBrowser-Private.sh
@@ -1,12 +1,10 @@
 #! /usr/bin/env bash
 
-if [ ! -z "$SUDO_USER" ]; then
-    BR_USER=$SUDO_USER
-else
-    BR_USER=$USER
+if "$USER" = "root"; then
+    exit "Browsers should definitely not run as root!"
 fi
 
-export DIR="/home/$BR_USER/.mozilla/firefox/firefox.profile.i2p"
+export DIR="$HOME/.mozilla/firefox/firefox.profile.i2p"
 
 "$SNAP/usr/lib/firefox.profile.i2p/install.sh" install
 "$SNAP/usr/lib/firefox.profile.i2p/install.sh" run
